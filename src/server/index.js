72d8ff102aee5201ccdb5d516c27ed9a800456bb
DashiellLumas <dashiell.lumas@gmail.com>
2018-08-20 12:15:14 -0400

    first commit

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const os = require('os');
-
+require('dotenv').config();
+const axios = require('axios');
+const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
-app.listen(8080, () => console.log('Listening on port 8080!'));
+
+
+app.listen(PORT, () => {
+  console.log("application listening on port:", PORT);
+})
